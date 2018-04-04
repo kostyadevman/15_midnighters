9fdd5d41d6f91c52659cbc6805087ae1214e5df1
kostya <kostra@localdomain>
2018-04-04 18:08:06 +0300

    Add requirements and update load_attempts function

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,28 @@
+import requests
+import pytz
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    devman_response = requests.get(
+        'https://devman.org/api/challenges/solution_attempts/?page=1',
+        params = {'page': '1'}
+    )
+    page_count = devman_response.json()['number_of_pages']
+    for page in range(1,page_count+1):
+        devman_response = requests.get(
+            'https://devman.org/api/challenges/solution_attempts/',
+            params={'page': '{}'.format(page)}
+        )
+        for attempt in devman_response.json()['records']:
+            yield attempt
+
+
 
 def get_midnighters():
     pass
 
+
 if __name__ == '__main__':
-  pass
+    attempts = load_attempts()
+    for attempt in attempts:
+        print(attempt)
