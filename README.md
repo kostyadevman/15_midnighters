5518c4cb4163f61012b36f24df9319f109501707
kostya <kostra@localdomain>
2018-04-07 21:27:05 +0300

    Update README, update get_midnighters function

--- README.md
+++ README.md
@@ -1,7 +1,31 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+Script will show you those people, who sent they solutions afther midnight until 06.00 in the morning
 
+# Example of script launch on Linux, Python 3.5:
+
+The script requires the installed Python interpreter version 3.5 and libraies **requests and pytz**
+
+````bash
+$ pip install -r requirements.txt
+````
+
+````bash
+$ python seek_dev_nighters.py
+av.bikkulova
+id311542249
+dreamfall3r
+oeananas
+khokhlov86
+paganismrus
+maxdmtrev
+a.biktop
+id441633628
+hoholsaniy
+IvanKumeyko
+id306803365
+````
 # Project Goals
+
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -2,39 +2,40 @@ import requests
 import pytz
 from datetime import datetime
 
+
 def load_attempts():
+    devman_url = 'https://devman.org/api/challenges/solution_attempts/'
     devman_response = requests.get(
-        'https://devman.org/api/challenges/solution_attempts/?page=1',
+        devman_url,
         params = {'page': '1'}
     )
     page_count = devman_response.json()['number_of_pages']
     for page in range(1,page_count+1):
         devman_response = requests.get(
-            'https://devman.org/api/challenges/solution_attempts/',
+            devman_url,
             params={'page': '{}'.format(page)}
         )
         for attempt in devman_response.json()['records']:
             yield attempt
 
 
-
 def get_midnighters(attempts):
+    midnighters = []
     for attempt in attempts:
         attempt_date = datetime.fromtimestamp(attempt['timestamp'])
         timezone = pytz.timezone(attempt['timezone'])
-        username = attempt['username']
+        midnighter = attempt['username']
 
         attempt_date_local = timezone.localize(attempt_date)
         midnite_hour = 0
         morning_hour = 6
         if midnite_hour <= attempt_date_local.hour < morning_hour:
-            yield username
+            if midnighter not in midnighters:
+                midnighters.append(midnighter)
+    return midnighters
 
-def print_midnighters(midnighters):
-    pass
 
 if __name__ == '__main__':
     attempts = load_attempts()
     midnighters = get_midnighters(attempts)
-    print_midnighters(midnighters)
-
+    print('\n'.join(midnighters))
